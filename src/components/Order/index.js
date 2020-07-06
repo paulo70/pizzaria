4b021a2004d088055e3cfb6d199facb76a0d3af6
paulo70 <paulofrontend@gmail.com>
2020-07-06 15:34:51 -0300

    clean function

--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import { Button, Form } from 'react-bootstrap'
 
 import './order.scss'
 
-function Order( navigation ){
-  const { previous, next } = navigation;
-
-  function handleSubmit(event){
-    event.preventDefault()
-  }
+function Order( ){
 
   return (
-    <Form onSubmit className='common-form'>
-      <div className='order'>
-        <h1 className='order-title'>Obrigado por comprar com a gente !!!</h1>
-        <h3 className='orde-subtitle'>Seu pedido está sendo processado em breve sera entregue</h3>
-      </div>
-    </Form>
+    <div className='order'>
+      <h1 className='order-title'>Obrigado por comprar com a gente !!!</h1>
+      <h3 className='orde-subtitle'>Seu pedido está sendo processado em breve sera entregue</h3>
+    </div>
   )
 }
 
